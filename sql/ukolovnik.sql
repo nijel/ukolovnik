1f727dbd2a5dcfd7a5847ac64a02679832053c2f
Michal Čihař <michal@cihar.com>
2006-01-14 12:48:48 +0000

    [ukolovnik @ michal@cihar.com--2005-main/ukolovnik--devel--1--patch-15] use phpMyAdmin for SQL dump - yes tool I work on has to be promoted ;-)

--- sql/ukolovnik.sql
+++ sql/ukolovnik.sql
@@ -1,23 +1,20 @@
--- MySQL dump 10.10
---
--- Host: localhost    Database: ukolovnik
--- ------------------------------------------------------
--- Server version	5.0.16-Debian_1-log
+-- phpMyAdmin SQL Dump
+-- version 2.7.1-dev
+-- http://www.phpmyadmin.net
+-- 
+-- Host: localhost
+-- Generation Time: Jan 14, 2006 at 01:47 PM
+-- Server version: 5.0.18
+-- PHP Version: 5.1.1-1
+-- 
+-- Database: `ukolovnik`
+-- 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- --------------------------------------------------------
 
---
+-- 
 -- Table structure for table `ukolovnik_categories`
---
+-- 
 
 DROP TABLE IF EXISTS `ukolovnik_categories`;
 CREATE TABLE `ukolovnik_categories` (
@@ -28,9 +25,11 @@ CREATE TABLE `ukolovnik_categories` (
   KEY `personal` (`personal`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
+-- --------------------------------------------------------
+
+-- 
 -- Table structure for table `ukolovnik_tasks`
---
+-- 
 
 DROP TABLE IF EXISTS `ukolovnik_tasks`;
 CREATE TABLE `ukolovnik_tasks` (
@@ -46,13 +45,3 @@ CREATE TABLE `ukolovnik_tasks` (
   KEY `category` (`category`),
   KEY `priority` (`priority`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
