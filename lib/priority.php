393b52e7f9f727b7baeb75cfc18b76b7e299a917
Michal Čihař <michal@cihar.com>
2006-03-04 14:07:32 +0000

    [ukolovnik @ michal@cihar.com--2005-main/ukolovnik--devel--1--patch-49] move priority hadling - priority stuff now has separate file

---
+++ lib/priority.php
@@ -0,0 +1,14 @@
+<?php
+// vim: expandtab sw=4 ts=4 sts=4:
+
+// This is priority handling stuff for Ukolovnik
+// Copyright (c) 2005-2006 Michal Čihař
+// Published under GNU GPL version 2
+
+// Grab needed libraries
+require_once('./lib/locale.php');
+
+function PRIORITY_grab() {
+    global $priorities;
+    $priorities = array(LOCALE_get('Priority0'), LOCALE_get('Priority1'), LOCALE_get('Priority2'));
+}
